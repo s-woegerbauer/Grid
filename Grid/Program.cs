655fd8344024e73d77290328dba7c51f8042ccf5
s-woegerbauer <120839159+s-woegerbauer@users.noreply.github.com>
2023-10-24 11:36:26 +0200

    Grid Initial
    - Added Grid.cs
    - Added MinHeap.cs
    - Added GetShortestPath to Grid
    - Tested with Advent Of Code 2022 Day 12 -> 10 minutes

---
+++ Grid/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Grid
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+            Console.BufferWidth = 1024;
+            Grid<char> grid = new(File.ReadAllLines(Directory.GetCurrentDirectory() + "\\input.txt"));
+            Console.WriteLine(grid.GetShortestPath('S', 'E', Barrier));
+        }
+
+        public static bool Barrier(char charOne, char charTwo)
+        {
+            if(charOne == 'S')
+            {
+                return false;
+            }
+
+            if(charTwo == 'E')
+            {
+                return charOne != 'z';
+            }
+
+            return charOne - charTwo < -1;
+        }
+    }
+}
